317573185154c882bf1998168e855cce7b8a9ed9
jayso <jcg5493@utulsa.edu>
2021-06-16 11:08:18 -0500

    initial release of cb

--- AIWolfPyGiven/aiwolfpy/contentbuilder.py
+++ AIWolfPyGiven/aiwolfpy/contentbuilder.py
@@ -1,4 +1,5 @@
 # cb is constructed according to regulations on http://aiwolf.org/control-panel/wp-content/uploads/2019/05/protocol_3_6.pdf
+# edited by Jayson C. Garrison
 
 # 2.1 should be good
 def estimate(subject, target, role):
@@ -7,7 +8,7 @@ def estimate(subject, target, role):
 def comingout(subject, target, role):
     return 'Agent[' + "{0:02d}".format(subject) + '] COMINGOUT Agent[' + "{0:02d}".format(target) + '] ' + role
 
-# 2.2
+# 2.2 should be good, the "I will do [action]"
 def divinization(subject, target):
     return 'Agent[' + "{0:02d}".format(subject) + '] DIVINIZATION Agent[' + "{0:02d}".format(target) + ']'
     
@@ -20,7 +21,7 @@ def vote(subject, target):
 def attack(subject, target):
     return 'Agent[' + "{0:02d}".format(subject) + '] ATTACK Agent[' + "{0:02d}".format(target) + ']'
 
-# 2.3 needs work
+# 2.3 should be good
 def divined(subject, target, species):
     return 'Agent[' + "{0:02d}".format(subject) + '] DIVINED Agent[' + "{0:02d}".format(target) + '] ' + species
 
@@ -30,12 +31,12 @@ def identified(subject, target, species):
 def guarded(subject, target):
     return 'Agent[' + "{0:02d}".format(subject) + '] GUARDED Agent[' + "{0:02d}".format(target) + ']'
 
-# 2.4 Needs work
+# 2.4 Needs work ,  talktype + ' day' + str(day) + ' ID:' + str(id)
 def agree(subject, talknumber):
-    return 'Agent[' + "{0:02d}".format(subject) + '] AGREE '+ talktype + ' day' + str(day) + ' ID:' + str(id)
+    return 'Agent[' + "{0:02d}".format(subject) + '] AGREE ' + str(talknumber)
 
 def disagree(subject, talknumber):
-    return 'Agent[' + "{0:02d}".format(subject) + '] DISAGREE '+ talktype + ' day' + str(day) + ' ID:' + str(id)
+    return 'Agent[' + "{0:02d}".format(subject) + '] DISAGREE ' + str(talknumber)
 
 # 2.5 fine
 def skip():
@@ -44,41 +45,47 @@ def skip():
 def over():
     return 'OVER'
 
-# 3 needs work
+# 3 should be good
 def request(subject, target, sentence):
-    return 'Agent[' + "{0:02d}".format(subject) + '] REQUEST(' + text + ''
+    return 'Agent[' + "{0:02d}".format(subject) + '] REQUEST Agent[' + "{0:02d}".format(target) + '] ' + sentence
 
 
-# \/ NEW PROTOCOLS CODE BELOW \/
+# \/ NEW PROTOCOLS CODE BELOW \/ # All should be ok
 
-def voted(subject, target):
-    return 'Agent[' + "{0:02d}".format(subject) + '] '
+def voted(subject, target): # should be good
+    return 'Agent[' + "{0:02d}".format(subject) + '] VOTED Agent[' + "{0:02d}".format(target) + ']'
 
-def attacked(subject, target):
-    return 'Agent[' + "{0:02d}".format(subject) + '] '
+def attacked(subject, target): # should be good
+    return 'Agent[' + "{0:02d}".format(subject) + '] ATTACKED Agent[' + "{0:02d}".format(target) + ']'
 
-def inquire(subject, target, sentence):
-    return 'Agent[' + "{0:02d}".format(subject) + '] '
+def inquire(subject, target, sentence): # should be good
+    return 'Agent[' + "{0:02d}".format(subject) + '] INQUIRE Agent[' + "{0:02d}".format(target) + ']' + '(' + sentence + ') '
 
-def because(subject, sentence1, sentence2):
-    return 'Agent[' + "{0:02d}".format(subject) + '] '
+def because(subject, sentence1, sentence2): # should be good
+    return 'Agent[' + "{0:02d}".format(subject) + '] BECAUSE ' + '(' + sentence1 + ') ' '(' + sentence2 + ') '
 
 def logicaland(subject, sentences):
     # where sentences is a list of sentences such that:
     # X states A and B and C and... where A, B, C,... are sentences
     # and X is the agent
-    return 'Agent[' + "{0:02d}".format(subject) + '] '
+    andStr = 'Agent[' + "{0:02d}".format(subject) + '] AND '
+    for element in sentences:
+        andStr = andStr + '(' + element + ') '
+    return andStr
 def logicalor(subject, sentences):
     # same property as logicaland
-    return 'Agent[' + "{0:02d}".format(subject) + '] '
+    orStr = 'Agent[' + "{0:02d}".format(subject) + '] OR '
+    for element in sentences:
+        andStr = andStr + '(' + element + ') '
+    return orStr
 def logicalxor(subject, sentence1, sentence2):
-    return 'Agent[' + "{0:02d}".format(subject) + '] '
+    return 'Agent[' + "{0:02d}".format(subject) + '] XOR ' + '(' + sentence1 + ') ' '(' + sentence2 + ') '
 
 def logicalnot(subject, sentence):
-    return 'Agent[' + "{0:02d}".format(subject) + '] '
+    return 'Agent[' + "{0:02d}".format(subject) + '] NOT ' + '(' + sentence + ') '
 
 def day(subject, daynumber, sentence):
-    return 'Agent[' + "{0:02d}".format(subject) + '] '
+    return 'Agent[' + "{0:02d}".format(subject) + '] DAY ' + str(daynumber) + '(' + sentence + ') '
 
 
 
--- test.py
+++ test.py
@@ -4,3 +4,16 @@ print("Hello2")
 print("Hello3")
 print("Hello4")
 print("Hello end")
+
+sent = ['s', '1', 'de']
+
+def nd(target, sentences):
+    retS = 'Test ' + target
+    for elm in sentences:
+        retS = retS + elm + ' '
+    return retS
+
+print( nd('Billy ', sent ) )
+
+x = 25
+print("testSTR" + str(x) )
