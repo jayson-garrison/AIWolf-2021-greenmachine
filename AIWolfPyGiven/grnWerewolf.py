1ed0b9e3e882d9eb7a572d9db5b48d091f657617
Grey858 <grey.bodi@gmail.com>
2021-06-24 15:31:22 -0500

    local changes grey (whisper)

--- AIWolfPyGiven/grnWerewolf.py
+++ AIWolfPyGiven/grnWerewolf.py
@@ -5,6 +5,7 @@
 import aiwolfpy
 from aiwolfpy import contentbuilder as cb
 import grnVillager
+import logging, json
 
 class Werewolf(grnVillager.Villager):
     def __init__(self, my_name):
@@ -17,6 +18,8 @@ class Werewolf(grnVillager.Villager):
         self.seer_target = set()
         self.bodyguard_target = set()
         self.nthWhisper = -1
+        self.daily_vote = -1
+        self.daily_estimate = -1
 
     def update(self, base_info, diff_data, request):
         super().update(base_info, diff_data, request)
@@ -24,6 +27,8 @@ class Werewolf(grnVillager.Villager):
         # secondary reset
         if not (self.currentDay == int( self.base_info['day']) ):
             self.nthWhisper = -1
+            self.daily_vote = -1
+            self.daily_estimate = -1
 
         # parse whisper
 
@@ -35,6 +40,8 @@ class Werewolf(grnVillager.Villager):
 
         # heuristics
 
+        
+
 
 
     def dayStart(self):
@@ -44,7 +51,24 @@ class Werewolf(grnVillager.Villager):
         return "Over"
 
     def vote(self): # new
-        return 1
+        logging.debug("# VOTE")
+        max = -1
+        maxWW = -1
+        vote = -1
+        voteWW = -1
+        
+        for player in self.estimate_votes:
+            if len(self.estimate_votes[player]) > max and player not in self.WWs: #debug try "and not (player in self.WWs)"
+                max = len(self.estimate_votes[player])
+                vote = player
+                if player in self.alive.union(self.likely_werewolf):
+                    maxWW = len(self.estimate_votes[player])
+                    voteWW = player
+        if vote in self.likely_werewolf or self.currentDay < 4:
+            return vote
+
+        else:
+            return voteWW
 
     def divine(self):
         pass
@@ -53,6 +77,16 @@ class Werewolf(grnVillager.Villager):
         return 1
 
     def whisper(self): # new
+        if not self.hasCO:
+            self.hasCO = True
+            return cb.comingout(self.idx, self.idx, "VILLAGER")
+
+        if len(proposedAttack)
+        elif self.currentDay < 3:
+            pass
+            #vote random villiager
+        
+        
         return "Over"
 
     def attack(self): # new
