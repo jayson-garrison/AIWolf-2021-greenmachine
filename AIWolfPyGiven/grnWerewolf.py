5aaa558fe7a8bba60711c30e688eb81f91092c24
jayso <jcg5493@utulsa.edu>
2021-06-24 15:29:03 -0500

    WW additions, parsing whisper, not stable

--- AIWolfPyGiven/grnWerewolf.py
+++ AIWolfPyGiven/grnWerewolf.py
@@ -17,6 +17,12 @@ class Werewolf(grnVillager.Villager):
         self.seer_target = set()
         self.bodyguard_target = set()
         self.nthWhisper = -1
+        self.allyCOSeer = False
+        self.allyCOMedium = False
+        self.attackVote = []
+        self.ally_whispers = []
+        self.allyProposedAttack = []
+
 
     def update(self, base_info, diff_data, request):
         super().update(base_info, diff_data, request)
@@ -24,11 +30,53 @@ class Werewolf(grnVillager.Villager):
         # secondary reset
         if not (self.currentDay == int( self.base_info['day']) ):
             self.nthWhisper = -1
+            self.ally_whispers = []
+            self.attackVote = []
+            self.allyProposedAttack = []
+            
 
         # parse whisper
-
-        #
+        for row in diff_data.itertuples():
+            type = getattr(row,"type")
+            text = getattr(row,"text")
+            if type == 'whisper':
+            # update the talk list
+                if type == 'talk': # then gather the text
+                    if self.currentDay != 0 and not ('SKIP' in text.upper() or 'OVER' in text.upper() ):
+                        whisperList = [int(getattr(row, 'turn')), int( getattr(row, 'agent') ) , str(text) ]
+                        self.ally_whispers.append(whisperList)
+                        print('new talks')
+                    else:
+                        whisperList = [int(getattr(row, 'turn')), int( getattr(row, 'agent') ) , str(text) ]
+                        self.ally_whispers.append(whisperList)
+                        print('new talks')
         
+        while self.nthWhisper < len(self.ally_whispers and not len(self.ally_whispers) == 0):
+
+            self.nthWhisper += 1
+
+            if "VOTE" in self.ally_whispers[self.nthTalk][2]:
+                self.attackVote.append([ self.ally_whispers[self.nthWhisper][1], self.ally_whispers[self.nthWishper][2][11:13] ])
+
+            elif "COMINGOUT" in self.ally_whispers[self.nthTalk][2]:
+                who = self.ally_whispers[self.nthTalk][1]
+                # print(who)
+                subject = int( self.ally_whispers[self.nthTalk][2][16:18] )
+                # print(subject)
+                if who == subject:
+                    if self.ally_whispers[self.nthTalk][2][20:] == "SEER":
+                        self.allyCOSeer = True
+                    if self.ally_whispers[self.nthTalk][2][20:] == "MEDIUM":
+                        self.allyCOMedium = True
+
+            elif "ESTIMATE" in self.ally_whispers[self.nthTalk][2]:
+                pass
+
+        # update ally alive status
+        for ally in self.WWs:
+            if ally not in self.alive:
+                self.WWs.remove(ally)
+
         #
 
         #
